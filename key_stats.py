abd732d354a7389ec1fa0a6ee593e8d642be7c1f
Joseph T Lyons <JosephTLyons@gmail.com>
2020-08-09 15:50:07 -0400

    Rename key to letter, make whitespace_character_substitutes

--- key_stats.py
+++ key_stats.py
@@ -3,82 +3,78 @@
 from pathlib import Path
 
 
-def get_key_to_occurrence_dict(input_string, should_include_whitespace):
-    key_to_occurrence_dict = {}
-    whitespace_characters = [" ", "\n"]
+def get_character_to_occurrence_dict(input_string, should_include_whitespace):
+    character_to_occurrence_dict = {}
+    whitespace_character_substitutes = {
+        " ": "SPACE",
+        "\n": "\\n"
+    }
 
-    for key in input_string:
-        if key in whitespace_characters:
-            if not should_include_whitespace:
-                continue
+    for character in input_string:
+        character = whitespace_character_substitutes.get(character, character)
 
-            if key == " ":
-                key = "SPACE"
-            elif key == "\n":
-                key = "\\n"
-
-        if key in key_to_occurrence_dict:
-            key_to_occurrence_dict[key] += 1
+        if character in character_to_occurrence_dict:
+            character_to_occurrence_dict[character] += 1
         else:
-            key_to_occurrence_dict[key] = 1
+            character_to_occurrence_dict[character] = 1
 
-    return key_to_occurrence_dict
+    return character_to_occurrence_dict
 
 
-def get_occurrence_to_key_list_dict(input_string, should_include_whitespace):
-    key_to_occurrence_dict = get_key_to_occurrence_dict(input_string, should_include_whitespace)
+def get_occurrence_to_character_list_dict(input_string, should_include_whitespace):
+    character_to_occurrence_dict = get_character_to_occurrence_dict(input_string, should_include_whitespace)
 
-    occurrence_to_key_list_dict = {}
+    occurrence_to_character_list_dict = {}
 
-    for key, occurrence in key_to_occurrence_dict.items():
-        if occurrence in occurrence_to_key_list_dict:
-            occurrence_to_key_list_dict[occurrence].append(key)
+    for character, occurrence in character_to_occurrence_dict.items():
+        if occurrence in occurrence_to_character_list_dict:
+            occurrence_to_character_list_dict[occurrence].append(character)
         else:
-            occurrence_to_key_list_dict[occurrence] = [key]
+            occurrence_to_character_list_dict[occurrence] = [character]
 
-    return occurrence_to_key_list_dict
+    return occurrence_to_character_list_dict
 
 
-def get_printing_items(sorted_occurrence_key_list_tuple_list):
-    total_keys_entered = 0
+def get_printing_items(sorted_occurrence_character_list_tuple_list):
+    total_characters_entered = 0
     longest_occurrence_len = 0
-    longest_keys_name_len = 0
+    longest_character_list_string = 0
 
-    for occurrence, key_list in sorted_occurrence_key_list_tuple_list:
-        total_keys_entered += occurrence * len(key_list)
+    for occurrence, character_list in sorted_occurrence_character_list_tuple_list:
+        total_characters_entered += occurrence * len(character_list)
         length_of_occurrence = len(str(occurrence))
 
         if length_of_occurrence > longest_occurrence_len:
             longest_occurrence_len = length_of_occurrence
 
         # Store this somewhere so it doesn't have to be computed again
-        keys_name = " ".join(key_list)
+        character_list_string = " ".join(character_list)
 
-        length_of_keys_name = len(keys_name)
+        length_of_character_list_string = len(character_list_string)
 
-        if length_of_keys_name > longest_keys_name_len:
-            longest_keys_name_len = length_of_keys_name
+        if length_of_character_list_string > longest_character_list_string:
+            longest_character_list_string = length_of_character_list_string
 
-    return total_keys_entered, longest_occurrence_len, longest_keys_name_len
+    return total_characters_entered, longest_occurrence_len, longest_character_list_string
 
 
-def print_histogram_and_details(sorted_occurrence_key_list_tuple_list, number_of_characters_for_full_histogram_bar):
-    total_keys_entered, longest_occurrence_len, longest_keys_name_len = get_printing_items(sorted_occurrence_key_list_tuple_list)
-    repetitions_of_most_used_key = sorted_occurrence_key_list_tuple_list[0][0]
+def print_histogram_and_details(sorted_occurrence_character_list_tuple_list, number_of_characters_for_full_histogram_bar):
+    total_characters_entered, longest_occurrence_len, longest_character_list_string = get_printing_items(sorted_occurrence_character_list_tuple_list)
+    repetitions_of_most_used_character = sorted_occurrence_character_list_tuple_list[0][0]
 
     print()
 
-    for occurrence, key_list in sorted_occurrence_key_list_tuple_list:
-        keys_name = " ".join(key_list)
-        padded_keys_string = str(keys_name).rjust(longest_keys_name_len, " ")
+    for occurrence, character_list in sorted_occurrence_character_list_tuple_list:
+        character_list_string = " ".join(character_list)
+        padded_character_list_string = str(character_list_string).rjust(longest_character_list_string, " ")
         padded_occurrence_string = str(occurrence).rjust(longest_occurrence_len, " ")
-        percentage_of_histogram_bar_to_print = occurrence / repetitions_of_most_used_key
+        percentage_of_histogram_bar_to_print = occurrence / repetitions_of_most_used_character
         histogram_bar = "*" * int(percentage_of_histogram_bar_to_print * number_of_characters_for_full_histogram_bar)
 
-        print(f"{padded_keys_string} | {padded_occurrence_string} | {histogram_bar}")
+        print(f"{padded_character_list_string} | {padded_occurrence_string} | {histogram_bar}")
 
     print()
-    print("Total keys Entered:", total_keys_entered)
+    print("Total Characters Entered:", total_characters_entered)
     print()
 
 
@@ -107,11 +103,11 @@ def main():
     print()
 
     input_string = get_input_string()
-    occurrence_to_key_list_dict = get_occurrence_to_key_list_dict(input_string, True)
+    occurrence_to_character_list_dict = get_occurrence_to_character_list_dict(input_string, True)
 
-    if occurrence_to_key_list_dict:
-        sorted_occurrence_key_list_tuple_list = sorted(occurrence_to_key_list_dict.items(), key=lambda x: x[0], reverse=True)
-        print_histogram_and_details(sorted_occurrence_key_list_tuple_list, 100)
+    if occurrence_to_character_list_dict:
+        sorted_occurrence_character_list_tuple_list = sorted(occurrence_to_character_list_dict.items(), key=lambda x: x[0], reverse=True)
+        print_histogram_and_details(sorted_occurrence_character_list_tuple_list, 100)
 
 
 if __name__ == "__main__":
