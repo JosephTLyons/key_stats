2312479da31814d9ed1600a918322b6f7fa3ab7a
Joseph T Lyons <JosephTLyons@gmail.com>
2020-09-28 23:56:19 -0400

    Use Black formatter

--- key_stats.py
+++ key_stats.py
@@ -4,10 +4,7 @@ from pathlib import Path
 
 
 def get_character_to_occurrence_dict(input_string, should_include_whitespace):
-    whitespace_character_substitutes = {
-        " ": "SPACE",
-        "\n": "\\n"
-    }
+    whitespace_character_substitutes = {" ": "SPACE", "\n": "\\n"}
     character_to_occurrence_dict = {}
 
     for character in input_string:
@@ -26,7 +23,9 @@ def get_character_to_occurrence_dict(input_string, should_include_whitespace):
 
 
 def get_occurrence_to_character_list_dict(input_string, should_include_whitespace):
-    character_to_occurrence_dict = get_character_to_occurrence_dict(input_string, should_include_whitespace)
+    character_to_occurrence_dict = get_character_to_occurrence_dict(
+        input_string, should_include_whitespace
+    )
     occurrence_to_character_list_dict = {}
 
     for character, occurrence in character_to_occurrence_dict.items():
@@ -61,18 +60,28 @@ def get_printing_items(sorted_occurrence_character_list_tuple_list):
     return total_characters_entered, longest_occurrence_len, longest_character_list_string
 
 
-def print_histogram_and_details(sorted_occurrence_character_list_tuple_list, number_of_characters_for_full_histogram_bar):
-    total_characters_entered, longest_occurrence_len, longest_character_list_string = get_printing_items(sorted_occurrence_character_list_tuple_list)
+def print_histogram_and_details(
+    sorted_occurrence_character_list_tuple_list, number_of_characters_for_full_histogram_bar
+):
+    (
+        total_characters_entered,
+        longest_occurrence_len,
+        longest_character_list_string,
+    ) = get_printing_items(sorted_occurrence_character_list_tuple_list)
     repetitions_of_most_used_character = sorted_occurrence_character_list_tuple_list[0][0]
 
     print()
 
     for occurrence, character_list in sorted_occurrence_character_list_tuple_list:
         character_list_string = " ".join(character_list)
-        padded_character_list_string = str(character_list_string).rjust(longest_character_list_string, " ")
+        padded_character_list_string = str(character_list_string).rjust(
+            longest_character_list_string, " "
+        )
         padded_occurrence_string = str(occurrence).rjust(longest_occurrence_len, " ")
         percentage_of_histogram_bar_to_print = occurrence / repetitions_of_most_used_character
-        histogram_bar = "*" * int(percentage_of_histogram_bar_to_print * number_of_characters_for_full_histogram_bar)
+        histogram_bar = "*" * int(
+            percentage_of_histogram_bar_to_print * number_of_characters_for_full_histogram_bar
+        )
 
         output = f"{padded_character_list_string} | {padded_occurrence_string} | {histogram_bar}".rstrip()
 
@@ -111,7 +120,9 @@ def main():
     occurrence_to_character_list_dict = get_occurrence_to_character_list_dict(input_string, True)
 
     if occurrence_to_character_list_dict:
-        sorted_occurrence_character_list_tuple_list = sorted(occurrence_to_character_list_dict.items(), key=lambda x: x[0], reverse=True)
+        sorted_occurrence_character_list_tuple_list = sorted(
+            occurrence_to_character_list_dict.items(), key=lambda x: x[0], reverse=True
+        )
         print_histogram_and_details(sorted_occurrence_character_list_tuple_list, 100)
 
 
